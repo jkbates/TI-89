0849e9172bb4b5a4e3057135a6300aefafde22fb
Jack Bates <jkb1473@g.rit.edu>
2024-05-06 21:07:16 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtUiTools import QUiLoader
+
+loader = QUiLoader()
+
+app = QtWidgets.QApplication(sys.argv)
+window = loader.load("mainwindow.ui", None)
+window.show()
+app.exec()
